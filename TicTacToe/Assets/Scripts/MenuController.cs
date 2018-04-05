d2ad1880c1bb1c15970ecdcdc974833e493aa78e
Cabamacadaf <maccus9@hotmail.com>
2018-04-05 17:42:18 +0200

    moar graphics

--- TicTacToe/Assets/Scripts/MenuController.cs
+++ TicTacToe/Assets/Scripts/MenuController.cs
@@ -5,11 +5,23 @@ using UnityEngine.SceneManagement;
 
 public class MenuController : MonoBehaviour {
 
+    public Animator animStart;
+    public Animator animExit;
+
     public void StartGame () {
+        animStart.SetTrigger("Play");
+        Invoke("StartTheGame", 2f);
+    }
+
+    void StartTheGame () {
         SceneManager.LoadScene("Game");
     }
 
     public void ExitGame () {
+        animExit.SetTrigger("Play");
+        Invoke("Exit", 2f);
+    }
+    void Exit () {
         Application.Quit();
     }
 }
