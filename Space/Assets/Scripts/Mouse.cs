263a207efbc132d4703c2afb0409e8ff2d192f1a
Cabamacadaf <maccus9@hotmail.com>
2018-04-12 14:30:45 +0200

    Lade till en fyrkant som man kan flytta på

---
+++ Space/Assets/Scripts/Mouse.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mouse : MonoBehaviour {
+    
+	void Update () {
+        if (Input.GetMouseButton(0)) {
+            Vector3 newPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            newPosition.z = 0;
+            transform.position = newPosition;
+        }
+	}
+}
---
+++ Space/Assets/Scripts/Touch.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Touch : MonoBehaviour {
+    
+	void Update () {
+		if(Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved) {
+            transform.position = Input.GetTouch(0).position;
+        }
+	}
+}
